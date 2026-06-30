1ca79cc75b0104785537cfa3fb2976cd7187cc6d
agungset0641 <8703389@gmail.com>
2026-06-30 14:52:45 +0700

    change to local css

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'gray-bg': '#F9FAFB',
+        'gray-text': '#1F2937',
+        'gray-accent': '#6B7280',
+      },
+      fontFamily: {
+        'spectral': ['Spectral', 'serif'],
+        'inter': ['Inter', 'sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+}
